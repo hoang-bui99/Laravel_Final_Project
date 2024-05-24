05c5315e1db31b0d468f2576a1bf6fb7a99803c9
hoang <buiduchoang2101@gmail.com>
2024-05-24 17:52:54 +0700

    laravel_final

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Order;
 
 class User extends Authenticatable
 {
@@ -19,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'email_verified_at',
         'password',
     ];
 
@@ -43,5 +45,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function orders(){
+        return $this->hasMany(Order::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,44 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\LiveWire\HomePage;
+use App\LiveWire\CategoriesPage;
+use App\LiveWire\ProductsPage;
+use App\LiveWire\CartPage;
+use App\LiveWire\ProductDetailPage;
+use App\LiveWire\CheckoutPage;
+use App\LiveWire\MyOrdersPage;
+use App\LiveWire\SuccessPage;
+use App\LiveWire\CancelPage;
+use App\LiveWire\MyOrderDetailPage;
+use App\LiveWire\Auth\LoginPage;
+use App\LiveWire\Auth\RegisterPage;
+use App\LiveWire\Auth\ForgotPasswordPage;
+use App\LiveWire\Auth\ResetPasswordPage;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductsPage::class);
+Route::get('/cart', CartPage::class);
+Route::get('/products/{slug}', ProductDetailPage::class);
+
+Route::middleware('guest')->group(function(){
+    Route::get('/login', LoginPage::class)->name('login');
+    Route::get('/register', RegisterPage::class);
+    Route::get('/forgot', ForgotPasswordPage::class);
+    Route::get('/reset', ResetPasswordPage::class);
 });
+
+Route::middleware('auth')->group(function(){
+    Route::get('/logout', function(){
+        auth()->logout();
+        return redirect('/');
+    });
+    Route::get('/checkout', CheckoutPage::class);
+    Route::get('my-orders', MyOrdersPage::class);
+    Route::get('my-orders/{order}', MyOrderDetailPage::class);
+    Route::get('/success', SuccessPage::class);
+    Route::get('/cancel', CancelPage::class);
+});
+
